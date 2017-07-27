9941dec4cfb7867917f3a0192da5836ee44f7bef
liuyunbin <liuyb4016@qq.com>
2017-07-27 15:08:00 +0800

    first

---
+++ src/main/java/com/eshore/yxt/media/core/util/media/PrintStream.java
@@ -0,0 +1,28 @@
+package com.eshore.yxt.media.core.util.media;
+
+public class PrintStream extends Thread
+{
+    java.io.InputStream __is = null;
+    public PrintStream(java.io.InputStream is)
+    {
+        __is = is;
+    }
+
+    public void run()
+    {
+        try
+        {
+            while(this != null)
+            {
+                int _ch = __is.read();
+                if(_ch != -1)
+                    System.out.print((char)_ch);
+                else break;
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
